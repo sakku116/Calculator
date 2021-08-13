7fd029120cd18d3e30d867531b97c97f800c7b99
sakku116 <88846365+sakku116@users.noreply.github.com>
2021-08-13 14:37:12 +0700

    first version
    simple system of sqrt probelm solving

--- main.py
+++ main.py
@@ -96,6 +96,7 @@ class CalcLayout(Widget):
         except:
             self.ids.output_label.text = f'ERROR'
 
+            
 class MyCalcApp(App):
     def build(self):
         return CalcLayout()
