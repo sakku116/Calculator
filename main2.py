1b3a79e3180fdbb6a6eb1f16e3fb49c7c6c5039e
Zakky <88846365+sakku116@users.noreply.github.com>
2021-08-28 12:50:34 +0700

    Add files via upload

--- main2.py
+++ main2.py
@@ -4,7 +4,10 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 import math
 
+# set window size
 Window.size = (270, 540)
+
+# load kv lang file
 Builder.load_file('calc_design.kv')
 
 class CalcLayout(Widget):
@@ -23,6 +26,7 @@ class CalcLayout(Widget):
         else:
             # menghapus karakter terakhir
             output = output[:-1]
+
             self.ids.output_label.text = output
 
     def button_press(self, char):
@@ -106,63 +110,12 @@ class CalcLayout(Widget):
         print(f'openBracket_index = {openBracket_index}', f'closeBracket_index = {closeBracket_index}')
 
         # replace semua karaker di dalam dictionary
-        replace_chars = {"×": "*",
+        replace_chars = {
+            "×": "*",
             "÷":"/",
             "^":"**"}
         for i, j in replace_chars.items():
             output = output.replace(i, j)
-
-        ''' ### OLD EQUALS SYSTEM ###
-        try:
-            # menyelesaikan masalah sqrt
-            ################################
-            if '√' in output:
-                sqrt_index = output.find('√')
-                def sqrt_float_handler(num):
-                    if '.' in output: 
-                        return str(math.sqrt(float(num)))
-                    else:
-                        return str(math.sqrt(eval(num)))
-
-                if sqrt_index == 0:
-                    remove_sqrt = output.replace('√', '')
-                    print(f'number without sqrt: {remove_sqrt}')
-                    output = sqrt_float_handler(remove_sqrt)
-
-                else:
-                    # memisah antara soal sebelum sqrt dan setelah sqrt dan menghilangkan sqrt
-                    splited_sqrt_output = output.split('√')
-                    print(f'splited_sqrt_output: {splited_sqrt_output}')
-                    # menyelesaikan masalah setelah sqrt
-                    sqrt_solver = sqrt_float_handler(splited_sqrt_output[1])
-                    # mengganti soal setelah sqrt dengan jawaban masalah sqrt
-                    splited_sqrt_output[1] = sqrt_solver
-                    print(f'splited_sqrt_output after solved: {splited_sqrt_output}')
-                    # menggabungkan kedua soal dengan beberapa kondisi
-                    op_index = output[sqrt_index-1]
-                    print(f'char before sqrt: {op_index}')
-                    if op_index == '-' or op_index == '+' or op_index == '*' or op_index == '**' or op_index == '/' or op_index == '%':
-                        print('ada operator')
-                        output = ''.join(splited_sqrt_output)
-                    else:
-                        print('tidak ada operator')
-                        output = '*'.join(splited_sqrt_output)
-            else:
-                pass
-            print(f'output string: {output}')
-            ###################################
-
-            result = str(eval(output)).replace('.0','')
-            if '.' in result:
-                formating_float_result = result.split('.')
-                formating_float_result[0] = f"{int(formating_float_result[0]):,}"
-                result = '.'.join(formating_float_result)
-                self.ids.output_label.text = str(result)
-
-            else:
-                result = f"{int(result):,}"
-                self.ids.output_label.text = str(result)
-        '''
         
         if '√' in output:
             print(output.split('√'))
@@ -186,10 +139,9 @@ class CalcLayout(Widget):
             result = str(eval(sqrt_output)).replace('.0','')
             
             print(f'{output} = {result}')
-                
 
-class MyCalcApp(App):
+class MyCalculatorApp(App):
     def build(self):
         return CalcLayout()
 if __name__ == '__main__':
-    MyCalcApp().run()
+    MyCalculatorApp().run()
